efaaba65469bc35b400c0ed8b0ff54fdcc5c9473
Ikkiiiii <123062700+Ikkiiiii@users.noreply.github.com>
2023-01-19 09:56:45 +0100

    Add files via upload

---
+++ VoorbeeldConsolee/VoorbeeldConsolee/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoorbeeldConsolee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            
+            Console.WriteLine("YOOOO");
+            Console.WriteLine("Druk op een toets om het programma te eindigen.");
+            Console.ReadKey();
+        }
+    }
+}
